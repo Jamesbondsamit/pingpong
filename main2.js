583515b4f668cbb1d08c34bc5128f5abd1f42dd6
Jamesbondsamit <78743840+Jamesbondsamit@users.noreply.github.com>
2022-03-16 17:02:17 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,12 +1,27 @@
 function preload() {
-	world_start = loadSound("world_start.wav");
+	world_start = loadSound("ball_touch_paddel.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
 	canvas = createCanvas(1240,336);
-	instializeInSetup(mario);
+	video= createCapture(VIDEO)
+	video.size(600,300);
+	instializeInSetup(ping-pong);
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.om('pose',gotPoses);
+}
+function modelLoaded(){
+	console,log("model is loaded")
+}
+function gotPoses(results){
+	if(results.length>0){
+		handX=results[0].pose.hand.x;
+		handY=results[0].pose.hand.y;
+		console.log("handX= " + handX + ", handY= " + handY);
+	}
 }
 
 function draw() {
